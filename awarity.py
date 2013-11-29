2ea994e20f9f7f77a28d2b0f3adf00162c20d0d9
Franz <franz@ikubu.com>
2013-11-29 12:00:28 +0000

    Pulling in secrets

--- awarity.py
+++ awarity.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 
+import secrets
 import mechanize
 from linkedin import linkedin
 import time
 import random
 
-API_KEY ='KEY'
-API_SECRET = 'SECRET'
-RETURN_URL = 'http://localhost:8000'
-USER_TOKEN = 'USER'
-USER_SECRET = 'SECRET'
-
 def authenticate():
-        authentication = linkedin.LinkedInDeveloperAuthentication(API_KEY, API_SECRET, USER_TOKEN, USER_SECRET, RETURN_URL, linkedin.PERMISSIONS.enums.values())
+        authentication = linkedin.LinkedInDeveloperAuthentication(secrets.API_KEY, \
+                                                                  secrets.API_SECRET, \
+                                                                  secrets.USER_TOKEN, \
+                                                                  secrets.USER_SECRET, \
+                                                                  secrets.RETURN_URL, \
+                                                                  linkedin.PERMISSIONS.enums.values())
         application = linkedin.LinkedInApplication(authentication)
         return application
 
@@ -22,10 +22,9 @@ def startLinkedInBrowser():
         browser.open("https://www.linkedin.com/")
         browser.select_form(name="login")
 
-        browser["session_key"] = "EMAIL"
-        browser["session_password"] = "PASSWORD"
+        browser["session_key"] = secrets.LINKEDIN_USERNAME
+        browser["session_password"] = secrets.LINKEDIN_PASSWORD
         response = browser.submit()
-        #print response.read()
         return browser
 
 """
@@ -48,14 +47,6 @@ access_token = authentication.get_access_token();
 #print application.get_profile()
 #print application.get_connections()
 #print application.search_profile(selectors=[{'people':['first-name', 'last-name']}],params={'keywords':'nolan van heerden'})
-
-#for connection in connections[u'values']:
-#        print connection[u'firstName'] + " " + connection[u'lastName'] + " " + connection[u'id']
-
-
-#print application.get_profile(member_id=u'auTCzoMHtw')
-
-#
 
 application = authenticate()
 connections = application.get_connections()
